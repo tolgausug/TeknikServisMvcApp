e01edce6c211030153b6a95282fa1c8857e56279
tolgausug <tolgausug@gmail.com>
2017-09-06 20:57:15 +0300

    Register işlemleri

---
+++ TeknikServisWebApp/TS.Web.BLL/SiteSettings/Settings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Web.MODEL.ViewModels;
+
+namespace TS.Web.BLL.SiteSettings
+{
+    public class Settings
+    {
+        public static string SiteMail = "wissen502wissen@gmail.com";
+        public static string SiteMailPassword = "wissen502";
+        public static string SiteMailSmtpHost = "smtp.gmail.com";
+        public static int SiteMailSmtpPort = 587;
+        public static bool SiteMailEnableSsl = true;
+
+        public async static Task SendMail(MailModel model)
+        {
+            using (var smtp = new SmtpClient())
+            {
+                var message = new MailMessage();
+                message.To.Add(new MailAddress(model.To));
+                message.From = new MailAddress(SiteMail);
+                message.Subject = model.Subject;
+                message.IsBodyHtml = true;
+                message.Body = model.Message;
+                var credential = new NetworkCredential()
+                {
+                    UserName = SiteMail,
+                    Password = SiteMailPassword
+                };
+                smtp.Credentials = credential;
+                smtp.Host = SiteMailSmtpHost;
+                smtp.Port = SiteMailSmtpPort;
+                smtp.EnableSsl = SiteMailEnableSsl;
+                await smtp.SendMailAsync(message);
+            }
+        }
+        public static string UrlFormatConverter(string url)
+        {
+            string sonuc = url;
+            sonuc = sonuc.Replace("'", "");
+            sonuc = sonuc.Replace(" ", "-");
+            sonuc = sonuc.Replace("<", "");
+            sonuc = sonuc.Replace(">", "");
+            sonuc = sonuc.Replace("&", "");
+            sonuc = sonuc.Replace("[", "");
+            sonuc = sonuc.Replace("!", "");
+            sonuc = sonuc.Replace("]", "");
+            sonuc = sonuc.Replace("ı", "i");
+            sonuc = sonuc.Replace("ö", "o");
+            sonuc = sonuc.Replace("ü", "u");
+            sonuc = sonuc.Replace("ş", "s");
+            sonuc = sonuc.Replace("ç", "c");
+            sonuc = sonuc.Replace("ğ", "g");
+            sonuc = sonuc.Replace("İ", "I");
+            sonuc = sonuc.Replace("Ö", "O");
+            sonuc = sonuc.Replace("Ü", "U");
+            sonuc = sonuc.Replace("Ş", "S");
+            sonuc = sonuc.Replace("Ç", "C");
+            sonuc = sonuc.Replace("Ğ", "G");
+            sonuc = sonuc.Replace("|", "");
+            sonuc = sonuc.Replace(".", "-");
+            sonuc = sonuc.Replace("?", "-");
+            sonuc = sonuc.Replace(";", "-");
+            return sonuc;
+        }
+    }
+}
---
+++ TeknikServisWebApp/TS.Web.UI/Areas/Yonetim/Controllers/HesapController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TS.Web.MODEL.IdentityModels;
+using TS.Web.MODEL.ViewModels;
+using static TS.Web.BLL.Account.MembershipTools;
+using TS.Web.BLL.SiteSettings;
+namespace TS.Web.UI.Areas.Yonetim.Controllers
+{
+    public class HesapController : Controller
+    {
+        // GET: Yonetim/Hesap
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var userManager = NewUserManager();
+            var checkUser = userManager.FindByEmail(model.Email);
+            if (checkUser != null)
+            {
+                ModelState.AddModelError("", "Bu email adresi kullanılmaktadır");
+                return View(model);
+            }
+            checkUser = await userManager.FindByNameAsync(model.Username);
+            if (checkUser != null)
+            {
+                ModelState.AddModelError("", "Bu kullanici adı kullanılmaktadır");
+                return View(model);
+            }
+            var actcode = Guid.NewGuid().ToString().Replace("-", "");
+            var user = new ApplicationUser()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                Surname = model.Surname,
+                UserName = model.Username,
+                ActivationCode = actcode
+            };
+            bool adminMi = userManager.Users.Count() == 0;
+            var sonuc = await userManager.CreateAsync(user, model.ConfirmPassword);
+            if (sonuc.Succeeded)
+            {
+                if (adminMi)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+
+                }
+                else
+                {
+                    if (model.FirmaMi)
+                        userManager.AddToRole(user.Id, "Firma");
+                    else
+                        userManager.AddToRole(user.Id, "Musteri");
+                }
+                await Settings.SendMail(new MailModel()
+                {
+                    To = user.Email,
+                    Subject = "Hoşgeldiniz",
+                    Message = $"Merhaba {user.UserName}, <br/>Sisteme başarıyla kaydoldunuz<br/>Hesabınızı aktifleştirmek için <a href='{SiteUrl()}/hesap/aktivasyon?code={actcode}'>Aktivasyon Kodu</a>"
+                });
+                
+                return RedirectToAction("Giris", "Hesap"); // Değişecek
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı kayıt işleminde hata oluştu!");
+                return View(model);
+            }
+        }
+
+        public string SiteUrl()
+        {
+            return Request.Url.Scheme + Uri.SchemeDelimiter + Request.Url.Host +
+            (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+        }
+   
+    }
+}
