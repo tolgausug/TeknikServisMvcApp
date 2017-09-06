af70732e8d13a6817e24b3ea53178e95b0230064
Halil <Halil@HIE>
2017-09-06 10:56:16 +0300

    yönetim areası oluşturuldu

--- TeknikServisWebApp/TS.Web.BLL/Repository/Repository.cs
+++ TeknikServisWebApp/TS.Web.BLL/Repository/Repository.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TS.Web.MODEL.Entities;
 
 namespace TS.Web.BLL.Repository
 {
-    public class Repository
-    {
-
-    }
+    public class KategoriRepo : RepositoryBase<Kategori, int> { }
+    public class ArizaKaydiRepo : RepositoryBase<ArizaKaydi, int> { }
+    public class ArizaKaydiDetayRepo : RepositoryBase<ArizaKaydiDetay, int> { }
 }
